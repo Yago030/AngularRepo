e951f4fd5fb6bbcda7b782882d5f9a70515eb91c
Santiago Bergerat <santiagobergerat@gmail.com>
2024-09-06 02:14:07 -0300

    subiendo

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -1,33 +1,95 @@
-import { src, dest, watch, series } from 'gulp';
-import * as dartSass from 'sass';
-import gulpSass from 'gulp-sass';
+import path from 'path'
+import fs from 'fs'
+import { glob } from 'glob'
+import { src, dest, watch, series } from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
 
-const sass = gulpSass(dartSass);
+const sass = gulpSass(dartSass)
 
-import terser from "gulp-terser"
+import terser from 'gulp-terser'
+import sharp from 'sharp'
 
-export function js(done){
+export function js( done ) {
     src('src/js/app.js')
-    .pipe(terser())
-    .pipe(dest('dist/js'))
+        .pipe(terser())
+        .pipe( dest('build/js') ) 
+
     done()
 }
 
+export function css( done ) {
+    src('src/scss/app.scss', {sourcemaps: true})
+        .pipe( sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError) )
+        .pipe( dest('build/css', {sourcemaps: '.'}) )
 
-export function css(done) {
-    src('src/scss/app.scss', {sourcemaps : true})
-        .pipe(sass({
-            outputStyle: 'compressed',
-        }).on('error', sass.logError))
-        .pipe(dest('dist/css',{sourcemaps : true}));  // Corrección: Cambiado dist/css a 'dist/css'
+    done()
+}
 
+export async function crop(done) {
+    const inputFolder = 'src/img/gallery/full'
+    const outputFolder = 'src/img/gallery/thumb';
+    const width = 250;
+    const height = 180;
+    if (!fs.existsSync(outputFolder)) {
+        fs.mkdirSync(outputFolder, { recursive: true })
+    }
+    const images = fs.readdirSync(inputFolder).filter(file => {
+        return /\.(jpg)$/i.test(path.extname(file));
+    });
+    try {
+        images.forEach(file => {
+            const inputFile = path.join(inputFolder, file)
+            const outputFile = path.join(outputFolder, file)
+            sharp(inputFile) 
+                .resize(width, height, {
+                    position: 'centre'
+                })
+                .toFile(outputFile)
+        });
+
+        done()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function imagenes(done) {
+    const srcDir = './src/img';
+    const buildDir = './build/img';
+    const images =  await glob('./src/img/**/*{jpg,png}')
+
+    images.forEach(file => {
+        const relativePath = path.relative(srcDir, path.dirname(file));
+        const outputSubDir = path.join(buildDir, relativePath);
+        procesarImagenes(file, outputSubDir);
+    });
     done();
 }
 
-export function dev() {
-    watch('src/scss/js/**/*.js', js);
+function procesarImagenes(file, outputSubDir) {
+    if (!fs.existsSync(outputSubDir)) {
+        fs.mkdirSync(outputSubDir, { recursive: true })
+    }
+    const baseName = path.basename(file, path.extname(file))
+    const extName = path.extname(file)
+    const outputFile = path.join(outputSubDir, `${baseName}${extName}`)
+    const outputFileWebp = path.join(outputSubDir, `${baseName}.webp`)
+    const outputFileAvif = path.join(outputSubDir, `${baseName}.avif`)
+    
+
+    const options = { quality: 80 }
+    sharp(file).jpeg(options).toFile(outputFile)
+    sharp(file).webp(options).toFile(outputFileWebp)
+    sharp(file).avif().toFile(outputFileAvif)
 }
 
+export function dev() {
+    watch('src/scss/**/*.scss', css)
+    watch('src/js/**/*.js', js)
+    watch('src/img/**/*.{png,jpg}', imagenes)
+}
 
-
-export default series(js, css, dev)
+export default series( crop, js, css, imagenes, dev )
